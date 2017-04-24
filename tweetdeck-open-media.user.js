4ce1ef90eb2a7f93bcb0e0e6b8098f7bb8889472
kjwon15 <kjwonmail@gmail.com>
2017-04-25 02:44:02 +0900

    First commit

---
+++ tweetdeck-open-media.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Tweetdeck media opener
+// @namespace    https://kjwon15.net/tweetdeck-open-media
+// @version      0.1
+// @description  Press "o" to open media in selected tweet.
+// @author       Kjwon15
+// @match        https://tweetdeck.twitter.com/*
+// ==/UserScript==
+
+/* jshint esnext: false */
+/* jshint esversion: 6 */
+
+function openMedia() {
+  let selectedTweet = document.querySelector('.is-selected-tweet');
+  let firstMedia = selectedTweet.querySelector('.media-preview a');
+  if (firstMedia) {
+    firstMedia.click();
+  }
+}
+
+document.addEventListener('keypress', (event) => {
+  if (['textarea', 'input'].includes(document.activeElement.tagName.toLowerCase())) {
+    return;
+  }
+
+  let alt = event.altKey;
+  let ctrl = event.ctrlKey;
+  let shift = event.shiftKey;
+  let meta = event.metaKey;
+
+  if (!alt && !ctrl && !shift && !meta && event.key == 'o') {
+    openMedia();
+  }
+
+  // console.log(`alt: ${event.altKey} ctrl: ${event.ctrlKey} shift: ${event.shiftKey} meta: ${event.metaKey} keycode: ${event.key}`);
+});
+
